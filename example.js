a604526bc398c8eb8e6e8778c16e5c6c01901f5b
JKS5 <jokekhanson@gmail.com>
2021-10-18 15:36:17 +0900

    firstnum

--- example.js
+++ example.js
@@ -1,8 +1,8 @@
-let num1 = 5;
+let num1 = 999999999999999;
 let num2 = 7335; //숫자 changed
 let operator = "*";
 calculaete();
 
 function calculate(n1, n2, op) {
   let result = n1 * n2; //곱하기로 바꿈
-} //주석 추가
+} //주석www D가aljfkdd
